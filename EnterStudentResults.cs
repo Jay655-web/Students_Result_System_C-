09b584598fe7e2c002e3241bbc7935eac74cafa6
Adjei Edmond <adjeiedmond582@gmai.com>
2026-06-24 13:33:47 +0000

    Creation of files

---
+++ EnterStudentResults.cs
@@ -0,0 +1,40 @@
+static void EnterStudentResults(List<Student>students)
+{
+    Student s = new Student();
+
+    Console.Write("Enter Full name: ");
+    s.FullName = Console.ReadLine();
+
+    Console.Write("Enter Student ID: ");
+    s.StudentId = Console.ReadLine();
+
+    Console.Write("Enter Programme: ");
+    s.Programme = Console.ReadLine();
+
+    Console.Write("Enter Level: ");
+    s.Level = Console.ReadLine();
+
+    string[] courseNames = {"Programming with C#","Database Systems","Computer Networks","Web Development","Mathematics for Computing"} 
+
+    for(int i = 0; i < 5; i++)
+    {
+        int score;
+        bool valid = false;
+        do
+        {
+            Console.Write($"Enter score for {courseNames[i]}: ");
+            valid = int.TryParse(Console.ReadLine().out score);
+
+            if(!valid || score <= 0 || score >= 100)
+            {
+                Console.WriteLine("Invalid Score. Score must be between 0 and 100.");
+                valid = flase;
+            }
+        }while(!valid);
+    }
+
+    s.Score[i] = score;
+
+    students.Add(s);
+
+}
